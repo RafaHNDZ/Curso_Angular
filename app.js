37cedc3a9aad894c149538a2bfc91ab586f7c410
RafaHNDZ <rafa_hndz@outlook.com>
2017-11-07 20:41:50 -0600

    CRUD para Album

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var app = express();
 //Cargar rutas
 var user_routes = require('./routes/user');
 var artists_routes = require('./routes/artists');
+var album_routes = require('./routes/album');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json())
@@ -18,5 +19,6 @@ app.use(bodyParser.json())
 //Carga de rutas base
 app.use('/api', user_routes);
 app.use('/api', artists_routes);
+app.use('/api', album_routes);
 
 module.exports = app;
---
+++ controllers/album.js
@@ -0,0 +1,164 @@
+'use strct'
+
+var path = require('path');
+var fs = require('fs');
+var mongoosePaginate = require('mongoose-pagination');
+
+var Artits = require('../models/artist');
+var Album = require('../models/album');
+var Song = require('../models/song');
+
+function getAlbum(req, res){
+  albumId = req.params.id;
+  Album.findById(albumId).populate({path: 'artist'}).exec((err, album) => {
+    if(err){
+      res.status(500).send({message: 'Error en la petición'});
+    }else{
+      if(!album){
+        res.status(200).send({messaje: 'No existe el album'});
+      }else{
+        res.status(500).send({album});
+      }
+    }
+  });
+}
+
+function saveAlbum(req, res){
+  album = new Album();
+  var params = req.body;
+
+  album.title = params.title;
+  album.description = params.description;
+  album.year = params.year;
+  album.image = 'null';
+  album.artist = params.artist;
+
+  album.save((err, albumStored) => {
+    if(err){
+      res.status(500).send({message: 'Error en la petición'});
+    }else {
+      if(!albumStored){
+        res.status(404).send({message: 'No se ha guardado el album'});
+      }else{
+        res.status(200).send({album: albumStored});
+      }
+    }
+  });
+}
+
+function getAlbums(req, res){
+  artitsId = req.params.artist;
+  if(!artitsId){
+    //Obtener todos los albums
+    var find = Album.find({}).sort('year');
+  }else{
+    //Obtener los albums del artista especificado
+    var find = Album.find({artist: artitsId}).sort('year');
+  }
+  find.populate({path: 'artist'}).exec((err, albums) => {
+    if(err){
+      res.status(500).send({message: 'Error en la peticion'})
+    }else{
+      if(!albums){
+        res.status(404).send({message: 'No se encontraron albums'});
+      }else{
+        res.status(200).send({albums});
+      }
+    }
+  });
+}
+
+function updateAlbum(req, res){
+  var albumId = req.params.id;
+  var update = req.body;
+
+  Album.findByIdAndUpdate(albumId, update, (err, albumUpdated) => {
+    if(err){
+      res.status(500).send({message: 'Error en la petición'});
+    }else{
+      if(!albumUpdated){
+        res.status(404).send({message: 'No se pudo actualizar el album'});
+      }else{
+        res.status(200).send({albumUpdated});
+      }
+    }
+  });
+}
+
+function deleteAlbum(req, res){
+  var albumId = req.params.id;
+
+  Album.findByIdAndRemove(albumId, (err, albumRemoved) => {
+    if(err){
+      res.status(500).send({message: 'Error al eliminar albums'});
+    }else{
+      if(!albumRemoved){
+        res.status(404).send({message: 'Los albums no han sido eliminados'});
+      }else{
+        //Eliminar las canciones de los albums removiidos
+        Song.find({album: albumRemoved._id}).remove((err, songRemoved) => {
+          if(err){
+            res.status(500).send({message: 'Error al eliminar cancion'});
+          }else{
+            if(!songRemoved){
+              res.status(404).send({message: 'La cancion no se pudo eliminar'});
+            }else{
+              res.status(200).send({album: albumRemoved});
+            }
+          }
+        });
+      }
+    }
+  });
+}
+
+function uploadImage(req, res){
+  var albumId = req.params.id;
+  var file_name = 'No subido...';
+  //Comprobar que existe algun archivo
+  if(req.files){
+    //Procesar nombre de la imagen
+    var file_path = req.files.image.path;
+    var file_split = file_path.split('/');
+    var file_name = file_split[2];
+    //Procesar extencion
+    var ex_split = file_name.split('.');
+    var file_ext = ex_split[1];
+    //Comprobar estencion
+    if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif'){
+      Album.findByIdAndUpdate(albumId,{image:file_name}, (err, albumUpdated) => {
+        if(err){
+          res.status(200).send({message:'Error al actualizar la imagen'})
+        }else{
+          res.status(200).send({artist:albumUpdated});
+        }
+      });
+    }else{
+      res.status(200).send({message: 'La extencion del fichero no es valida'});
+    }
+    console.log(ex_split);
+  }else{
+    res.status(200).send({message: 'No has subido ninguna imagen'});
+  }
+}
+
+function getImageFile(req, res){
+  var imageFile = req.params.imageFile;
+  var path_file = './uploads/albums/'+imageFile;
+  fs.exists(path_file, function(exists){
+    if(exists){
+      res.sendFile(path.resolve(path_file));
+    }else{
+      res.status(200).send({message: 'No existe el fichero'});
+    }
+  });
+}
+module.exports = {
+  getAlbum,
+  saveAlbum,
+  getAlbums,
+  updateAlbum,
+  deleteAlbum,
+  uploadImage,
+  getImageFile
+}
